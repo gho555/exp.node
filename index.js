9b90961999cb1d4a91d95d6e16f315d494ef937d
alexzmye72 <alexzmye72@outlook.com>
2022-12-23 11:04:12 +0330

    vm-node-module

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const process = require('process');
 const app = require('express')();
 const zlib = require('zlib');
 const assert = require('assert');
+var vm = require('vm');
 
 ////////////////////////////////////////////fs///////////////////////////////////////////////////////////////////////////////
 
@@ -32,7 +33,6 @@ const assert = require('assert');
 //     if (pathName === '/' || pathName === '/overview') {
 //       res.end('this is the overview');
 //     }
-
 //     if (pathName === '/book' && method === 'POST') {
 //       res.writeHead(404, {
 //         'Content-type': 'text/html',
@@ -57,43 +57,33 @@ const assert = require('assert');
 
 // console.log(crypto.getHashes());
 // const log = crypto.getCiphers();
-
 // const hash = crypto.createHash('sha256').update('your massagre').digest('hex');
-
 // const hash = crypto.createHash('sha1');
 // let key = hash.digest().slice(0, 16);
-
 // const iv = crypto.randomBytes(16);
 // let key = '123645564564564654564564456789';
 // let secret_massage = ':)';
 // let cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
 // let encrypted = cipher.update(secret_massage, 'utf-8', 'hex');
 // encrypted += cipher.final('hex');
-
 // console.log('encrypted:' + encrypted);
-
 // let decipher = crypto.createCipheriv('aes-128-cbc', key, iv);
 // let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
 // decrypted += decipher.final('utf-8');
-
 // console.log('decrypted:' + decrypted);
-
 // console.log(vi);
 // console.log(hash);
-
 // cluster.isMaster;
 
 ////////////////////////////////////////////os///////////////////////////////////////////////////////////////////////////////
 // const numCpu = os.cpus().length;
 // console.log('numCpu:' + numCpu);
-
 // app.get('/', (req, res) => {
 //   for (let i = 0; i < ie8; i++) {
 //     //a infinitive loop for execute log process
 //   }
 //   res.send(`ok...${process.pid}`);
 // });
-
 // if (cluster.isMaster) {
 //   for (let i = 0; i < numCpu; i++) {
 //     cluster.fork();
@@ -109,7 +99,6 @@ const assert = require('assert');
 //     console.log(`running server ${process.pid} on 127.0.0.1:8000`),
 //   );
 // }
-
 // const totalMemory = os.totalmem();
 // const freeMemory = os.freemem();
 // const data = os.homedir();
@@ -121,7 +110,6 @@ const assert = require('assert');
 // const inp = fs.createReadStream('index.js.zip');
 // const out = fs.createWriteStream('index2.js');
 // // inp.pipe(gzip).pipe(out);
-
 // inp.pipe(unzip).pipe(out);
 
 ////////////////////////////////////////////path///////////////////////////////////////////////////////////////////////////////
@@ -132,28 +120,21 @@ const assert = require('assert');
 // function add(a, b) {
 //   return a + b;
 // }
-
 // function bigger(a, b) {
 //   return a > b;
 // }
-
 // var expected = add(1, 2);
 // var bigger = bigger(2, 1);
 // assert.equal(expected, 3, 'one plus two is three');
 // assert(bigger);
-
 // assert(2 < 1, 'two should be bigger than one');
-
 // assert(50 > 70, 'My message goes here');
-
 // assert(50 - 50);
-
 // const a = {
 //   person: {
 //     name: 'alireza',
 //   },
 // };
-
 // const b = {
 //   person: {
 //     name: 'alreza',
@@ -162,13 +143,42 @@ const assert = require('assert');
 
 // assert.notDeepEqual(a, b, 'not deep equal');
 
-////////////////////////////////////////////assert///////////////////////////////////////////////////////////////////////////////
-// const buf = Buffer.from('abc');
-const buf1 = Buffer.alloc(17);
-const buf2 = Buffer.alloc(16);
+////////////////////////////////////////////buffer///////////////////////////////////////////////////////////////////////////////
+// var buf1 = Buffer.from('abcdefghijkl');
+// var buf2 = Buffer.from('HELLO');
+// const buf1 = Buffer.alloc(1);
+// const buf2 = Buffer.alloc(3);
+// const buf3 = Buffer.alloc(2);
+// const buf4 = Buffer.allocUnsafe(0);
+// buf4.copy(buf, 2);
+// console.log(buf4.toString());
 // const buf = Buffer.allocUnsafe(15);
 // const buf = Buffer.allocUnsafeSlow(15);
 // const byt = Buffer.byteLength(buf);
-
-console.log(Buffer.compare(buf1, buf2));
+// const arr = [buf1, buf2, buf3];
+// console.log(arr);
 // console.log(byt);
+// const arrayConcat = Buffer.concat(arr);
+// console.log(arr.sort(Buffer.compare));
+// console.log(Buffer.byteLength(arrayConcat));
+// var buf1 = Buffer.from('abcdefghijkl');
+// var buf2 = Buffer.from('HELLO');
+// buf2.copy(buf1, 2);
+// console.log(buf2.toString());
+
+// buf2.copy(buf1, 2);
+// console.log(buf2.toString());
+// console.log(buf1.toString());
+// var buf1 = Buffer.from('hi');
+// var buf2 = Buffer.from('world');
+// buf2.copy(buf1);
+// console.log(buf1.toString());
+// console.log(buf2.toString());
+
+////////////////////////////////////////////vm///////////////////////////////////////////////////////////////////////////////
+var myObj = { name: 'John', age: 38 };
+vm.createContext(myObj);
+
+vm.runInContext('age += 1;', myObj);
+
+console.log(myObj);
