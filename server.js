cfa24b089cab5dc7dbb9bcdc843f89467d3678de
alexzmye72 <alexzmye72@outlook.com>
2022-12-25 23:21:36 +0330

    exp-server-file

--- server.js
+++ server.js
@@ -3,16 +3,55 @@ const url = require('url');
 var querystring = require('querystring');
 
 const server = http.createServer((req, res) => {
-  const { url: reqUrl, method } = req;
-  const { search, query, pathname } = url.parse(reqUrl, true);
+  // take url and method req
+  const { url: urlPath, method } = req;
+ 
+  // url.parse
+  const {
+    query,
+    protocol,
+    slashes,
+    auth,
+    host,
+    port,
+    hostname,
+    hash,
+    search,
+    pathname,
+    path,
+    href,
+  } = url.parse(urlPath, true);
+
+  //take a parameter from query
   const { name } = query;
-  console.log('name', name);
-  console.log({
-    method: method,
-    query: query,
-    pathName: pathname,
+
+  //set header
+  res.writeHead(404, {
+    'Content-type': 'text/json',
+    'my-own-header': 'hello world',
   });
-  res.end('ok');
+
+  //send result
+  res.end(
+    JSON.stringify({
+      method,
+      query,
+      urlPath,
+      protocol,
+      slashes,
+      auth,
+      host,
+      port,
+      hostname,
+      hash,
+      search,
+      query,
+      pathname,
+      path,
+      href,
+      name,
+    }),
+  );
 });
 
 server.listen(8080, () => {
