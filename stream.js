435e66c6eaad00a6012a9aaa1a0b4e22e4b198dc
alexzmye72 <alexzmye72@outlook.com>
2022-12-30 01:46:13 +0330

    token-jwt-exp

--- stream.js
+++ stream.js
@@ -1,16 +1,34 @@
 const fs = require('fs');
-const server = require('https').createServer();
+const server = require('http').createServer();
 
 server.on('request', (req, res) => {
   // Solution 1
-  fs.readFile('test-file.txt', (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
-  });
+  // fs.readFile('text.txt', (err, data) => {
+  //   if (err) console.log(err);
+  //   res.end(data);
+  // });
+
+  // Solution 2
+  // console.log('ok');
+  // const readable = fs.createReadStream('text.txt');
+  // readable.on('data', (chunk) => {
+  //   res.write(chunk);
+  // });
+
+  // readable.on('end', () => {
+  //   res.end('ok');
+  // });
+
+  // readable.on('error', (err) => {
+  //   console.log(err);
+  //   res.statusCode = 500;
+  //   res.end('file not find!');
+  // });
+
+  // solution 3
+  const readable = fs.createReadStream('text.txt');
+  readable.pipe(res);
 });
-
-// Solution 2
-
 
 server.listen(8000, '127.0.0.1', () => {
   console.log('listening...');
--- token.js
+++ token.js
@@ -1,1 +1,21 @@
-// hello world
+var jwt = require('jsonwebtoken');
+
+jwt.sign(
+  {
+    exp: Math.floor(Date.now() / 1000) + 60 * 60,
+    data: 'foobar',
+  },
+  privateKey,
+  { algorithm: 'RS256' },
+  function (err, token) {
+    console.log(token);
+  },
+  'secret',
+);
+
+// verify a token asymmetric
+var cert = fs.readFileSync('public.pem');
+jwt.verify(token, 'wrong-secret', function (err, decoded) {
+  // err
+  // decoded undefined
+});
